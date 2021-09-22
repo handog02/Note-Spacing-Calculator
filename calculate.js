a0e94676d46ba852ff16dea34873574ae435398d
handog02 <hanzhao47@gmail.com>
2021-09-22 16:28:41 +0100

    Added error message

--- calculate.js
+++ calculate.js
@@ -9,6 +9,21 @@ function calculate(){
     let noteSpace = 0;
     let otherSpace = 0;
 
+    // const lut = {
+    //     wd: ['w.', 'dotted whole', 40],
+    //     w: ['w', 'whole', 30],
+    //     hd: ['h.', 'dotted half', 24],
+    //     h: ['h', 'half', 20],
+    //     qd: ['q.', 'dotted quarter', 120/7],
+    //     q: ['q', 'quarter', 15],
+    //     ed: ['e.', 'dotted 8th', 40/3],
+    //     e: ['e', '8th', 12],
+    //     sd: ['dotted 16th', 120/11],
+    //     s: ['s', '16th', 10],
+    //     td: ['t.', 'dotted 32nd', 120/13],
+    //     t: ['t', '32nd', 60/7]
+    // };
+
     const addNoteSpace = n => {
         switch(n){
             case 'w.':
@@ -91,45 +106,51 @@ function calculate(){
     notesNew = [...new Set(notesNew)];
     const result = document.querySelector('#result');
     result.innerHTML = '';
+    const error = document.querySelector('#error');
+    error.innerHTML = '';
 
     notesNew.forEach(note => {
         switch(note){
             case 'w.':
-                result.innerHTML += `<li>dotted whole: ${unit*40} units</li>` 
+                result.innerHTML += `<li>dotted whole: ${unit*40} units</li>`;
                 break;
             case 'w':
-                result.innerHTML += `<li>whole: ${unit*30} units</li>` 
+                result.innerHTML += `<li>whole: ${unit*30} units</li>`; 
                 break;
             case 'h.':
-                result.innerHTML += `<li>dotted half: ${unit*24} units</li>`
+                result.innerHTML += `<li>dotted half: ${unit*24} units</li>`;
                 break;
             case 'h':
-                result.innerHTML += `<li>half: ${unit*20} units</li>`
+                result.innerHTML += `<li>half: ${unit*20} units</li>`;
                 break;
             case 'q.':
-                result.innerHTML += `<li>dotted quarter: ${unit*(120/7)} units</li>`
+                result.innerHTML += `<li>dotted quarter: ${unit*(120/7)} units</li>`;
                 break;
             case 'q':
-                result.innerHTML += `<li>quarter: ${unit*15} units</li>`
+                result.innerHTML += `<li>quarter: ${unit*15} units</li>`;
                 break;
             case 'e.':
-                result.innerHTML += `<li>dotted 8th: ${unit*(40/3)} units</li>`
+                result.innerHTML += `<li>dotted 8th: ${unit*(40/3)} units</li>`;
                 break;
             case 'e':
-                result.innerHTML += `<li>8th: ${unit*12} units</li>`
+                result.innerHTML += `<li>8th: ${unit*12} units</li>`;
                 break;
             case 's.':
-                result.innerHTML += `<li>dotted 16th: ${unit*(120/11)} units</li>`
+                result.innerHTML += `<li>dotted 16th: ${unit*(120/11)} units</li>`;
                 break;
             case 's':
-                result.innerHTML += `<li>16th: ${unit*10} units</li>`
+                result.innerHTML += `<li>16th: ${unit*10} units</li>`;
                 break;
             case 't.':
-                result.innerHTML += `<li>dotted 32nd: ${unit*(120/13)} units</li>`
+                result.innerHTML += `<li>dotted 32nd: ${unit*(120/13)} units</li>`;
                 break;
             case 't':
-                result.innerHTML += `<li>16th: ${unit*60/7} units</li`
+                result.innerHTML += `<li>16th: ${unit*60/7} units</li`;
         }
     })
+
+    if($('#result').is(':empty')){
+        error.innerHTML += 'Error: please check your input!'
+    }
 
 }
